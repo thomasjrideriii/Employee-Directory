b649a7d67fa79889e6d826bcf39f9b03c4ed8ac6
Thomas Rider <thomasjrideriii@gmail.com>
2020-07-06 13:42:24 -0400

    Added Table Format and rendered column titles

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -2,7 +2,19 @@ import React from "react";
 
 function EmployeeTable(props) {
     return(
-        <h1>Table Goes Here!</h1>
+        <table className="table table-dark">
+            <thead>
+            <tr>
+                <th scope="col">Thumbnail</th>
+                <th scope="col">Name</th>
+                <th scope="col">Date of Birth</th>
+                <th scope="col">City</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone Number</th>
+            </tr>
+            </thead>
+            {props.children}
+        </table>
     )
 }
 
